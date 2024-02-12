f57c12498fa0241cf6505d37383e6432f32f4ab4
Benjameslane <ben.james.lane9>
2024-02-12 13:45:28 -0700

    added bg animation and Welcome text to my portfolio

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      Discover how my unique blend of skills and projects can drive innovation and success in your team. This portfolio is more than a showcase; it's a testament to my dedication to excellence in development. Through each project, I demonstrate not only my technical abilities but also my capacity for creative problem-solving and adaptability. I'm committed to delivering value and achieving outstanding results. Let's explore how my expertise can contribute to the growth and success of your organization.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://github.com/Benjameslane?tab=repositories'}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
